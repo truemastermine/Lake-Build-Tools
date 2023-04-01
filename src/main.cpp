91635bb44f2b930d3553666c016f479bbb1c75e2
MastermineGames <josiahdbarbieri@gmail.com>
2023-03-31 18:53:18 -0700

    It is a start

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
+#include <filesystem>
+#include <vector>
 
-int main() { std::cout << "Hello, World!\n"; }
+#include "sol/sol.hpp"
+
+namespace fs = std::filesystem;
+
+void lmake(){}
+void project(){}
+std::vector<std::string> listDir(std::string_view dir, std::string_view ext){
+    std::vector<std::string> res;
+    for (const auto& file : fs::recursive_directory_iterator(dir)){
+        std::cout << file.path() << '\n';
+    }
+    return res;
+}
+
+int main() { 
+    sol::state lua;
+    listDir("/extra", "");
+
+}
