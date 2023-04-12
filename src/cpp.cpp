5fa333dafa99a6492853b7bd0ba7ff3486b45c27
MastermineGames <josiahdbarbieri@gmail.com>
2023-04-12 14:28:43 -0700

    Basic Static Linking

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -37,11 +37,54 @@ static void addLink(API::Project& p, API::Project& target, API::LinkType ty) {
     pro->libs.emplace_back(Link{.pro = targ, .type = (unsigned char)ty});
 }
 
+static void buildLink(Link& lnk){
+    auto pro = lnk.pro;
+    std::string_view bin = pro->cpp ? "g++ " : "gcc ";
+    std::string_view buildName = pro->id;
+
+    // INCLUDES
+    std::string incs;
+    for (auto& incDir : pro->incDirs) {
+        incs += " -I ";
+        incs += incDir;
+    }
+
+    // SOURCE TO OBJ
+    std::string srcs;
+    for (auto& srcFile : pro->srcs){
+        fs::path srcPath(srcFile);
+
+        // FS Checks
+        if (!(fs::exists(srcPath) && fs::is_regular_file(srcPath)))
+            throw "Expected File Not Found: " + srcFile;
+        if (fs::last_write_time(srcPath) < fs::last_write_time(srcPath)) continue;
+
+        std::cout << bin << "-c " << srcFile << " -Wall -o " << API::buildDir //<< '/'
+                  << srcPath.filename().replace_extension(".o").generic_string() << " " << incs
+                  << '\n';
+        srcs += ' ';
+        srcs += API::buildDir;
+        srcs += srcPath.filename().replace_extension(".o").generic_string();
+    }
+
+    std::cout << "ar rvs " << API::buildDir << buildName << ".a " << srcs << '\n';
+}
+
 static void buildProject(API::Project& p, std::string_view buildName) {
     auto pro = (CPPProject*)p.data;
     std::string_view bin = pro->cpp ? "g++ " : "gcc ";
     if (buildName.empty()) buildName = pro->id;
     // std::cout << pro.id << '\n';
+
+    // LINKS
+    std::string lnks;
+    for (auto& link : pro->libs){
+        buildLink(link);
+        lnks += " ";
+        lnks += API::buildDir;
+        lnks += link.pro->id;
+        lnks += ".a";
+    }
 
     // INCLUDES
     std::string incs;
@@ -69,7 +112,7 @@ static void buildProject(API::Project& p, std::string_view buildName) {
     }
 
     // FINAL COMPILATION
-    std::cout << bin << srcs << " -o " << API::buildDir << "LAKE" <<'\n';
+    std::cout << bin << srcs << lnks << " -o " << API::buildDir << "LAKE" <<'\n';
 }
 
 }  // namespace CPP
